b26ab831426d9e9d7c90b139e7c1f49c33260529
YangLichun-sustech <58096103+YangLichun-sustech@users.noreply.github.com>
2021-07-30 13:04:26 +0800

    Add files via upload

--- formCell2EdgeMatrix.m
+++ formCell2EdgeMatrix.m
@@ -1,36 +1,17 @@
-% Form the mapping matrix that transform cell conductivity model (cellCon in S/m)
-% to conductance on edges; mesh-independent.
-% function Cell2Edge = formCell2EdgeMatrix(edges,lengths,faces,cells,volumes)
-% INPUT
-%     edges:  a 2-column matrix of node index for the edges; 1st column for
-%     starting node and 2nd column for ending node
-%     lengths: a vector of the edges' lengths in meter
-%     faces: a 4-column matrix of edge index for the faces
-%     cells: a 6-column matrix of face index for the cells
-%     volumes: a vector of the cells' volume in cubic meter
-% OUTPUT
-%     Cell2Edge: a Nedges x Ncells matrix that is equivalent to cellVolume/length/length
-% NOTE
-%     Suppose cellCon is the cell conductivity vector defined on all of the
-%     cells. Cc = Cell2Edge * cellCon gets the conductances of the
-%     equivalent resistors defined on the edges. The total conductance from
-%     a cell is equally distributed to all its edges.
 function Cell2Edge = formCell2EdgeMatrix(edges,lengths,faces,cells,volumes)
 
-Nedges = size(edges,1);
-[Nfaces, Nepf] = size(faces); % # of faces and # of edges per face
-[Ncells, Nfpc] = size(cells); % # of cells and # of faces per cell
+Nnodes = 8;
+[Nedges,~]    = size(edges);
+[~,Nepf] = size(faces);
+[Ncells,Nfpc] = size(cells);
 
-% form Cell2Edge (matrix to be multiplied by cellCon)
-I1 = faces; % row index (which edge)
-J1 = repmat((1:Nfaces)',1,Nepf); % column index
-S1 = repmat(1/Nepf,Nfaces,Nepf); % % each edge receivers equal portion from face
-I2 = cells; % row index (which face)
-J2 = repmat((1:Ncells)',1,Nfpc); % column index
-S2 = repmat(volumes./Nfpc,1,Nfpc); % equivalent volume per edge - edges equally share the volume from a cell
-Cell2Edge = spdiags(1./lengths.^2,0,Nedges,Nedges) * ...
-            sparse(I1(:),J1(:),S1(:),Nedges,Nfaces) * ... % distribute volume from face to edge
-            sparse(I2(:),J2(:),S2(:),Nfaces,Ncells); % distribute volume from cell to face
-        
+J = faces( reshape(cells',[],1) , : );
+J = reshape(J',[],1);
+J = unique( reshape(J,Nepf*Nfpc,[]), 'rows' , 'stable' )';
+J = J(:);
+I = repmat( (1:Ncells)' , 1 , Nfpc+Nnodes-2 );
+I = I(:);
+
+Cell2Edge = spdiags(1./lengths.^2,0,Nedges,Nedges) * sparse(J,I,1,Nedges,Ncells) * spdiags(volumes./4,0,Ncells,Ncells);
+
 end
-
--- formFace2EdgeMatrix.m
+++ formFace2EdgeMatrix.m
@@ -1,30 +1,12 @@
-% Form the mapping matrix that transform face conductivity model (faceCon in S)
-% to conductance on edges; mesh-independent.
-% function Face2Edge = formFace2EdgeMatrix(edges,lengths,faces,areas)
-% INPUT
-%     edges:  a 2-column matrix of node index for the edges; 1st column for
-%     starting node and 2nd column for ending node
-%     lengths: a vector of the edges' lengths in meter
-%     faces: a 4-column matrix of edge index for the faces
-%     areas: a vector of the faces' area in square meter
-% OUTPUT
-%     Face2Edge: a Nedges x Nfaces matrix that is equivalent to faceArea/length/length
-% NOTE
-%     Suppose faceCon is the face conductivity vector defined on all of the
-%     faces. Cf = Face2Edge * faceCon gets the conductances of the
-%     equivalent resistors defined on the edges. The total conductance from
-%     a face is equally distributed to all its edges.
+
 function Face2Edge = formFace2EdgeMatrix(edges,lengths,faces,areas)
 
-Nedges = size(edges,1);
-[Nfaces, Nepf] = size(faces); % # of faces and # of edges per face
+[Nedges,~] = size(edges);
+[Nfaces,Nepf] = size(faces);
 
-% form Face2Edge (matrix to be multiplied by faceCon)
-I = faces; % row index (which edge)
-J = repmat((1:Nfaces)',1,Nepf); % column index
-S = repmat(areas./Nepf,1,Nepf); % equivalent area per edge - edges equally share the area from a face
-Face2Edge = spdiags(1./lengths.^2,0,Nedges,Nedges) * sparse(I(:),J(:),S(:),Nedges,Nfaces); % distribute area from face to edge
+I = reshape( repmat((1:Nfaces)',1,Nepf)' ,[] ,1 );
+J = reshape(faces',[], 1);
 
-
+Face2Edge = spdiags(1./lengths.^2,0,Nedges,Nedges) * sparse(J,I,1,Nedges,Nfaces) * spdiags(areas./2,0,Nfaces,Nfaces);
 
 end
